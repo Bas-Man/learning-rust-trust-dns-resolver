27d76f0debfc6c4b0566cebf053199dc0c73954d
Adam Spann <Bas-Man@users.noreply.github.com>
2021-04-11 23:31:56 +0900

    Started working with some basic tests

--- src/dns/mx.rs
+++ src/dns/mx.rs
@@ -1,7 +1,59 @@
+use ipnetwork::IpNetwork;
+use std::ptr;
 use trust_dns_resolver::config::*;
 use trust_dns_resolver::error::ResolveResult;
 use trust_dns_resolver::lookup::MxLookup;
 use trust_dns_resolver::Resolver;
+
+/// Contains host priority and exchange host name
+pub struct MxHost {
+    priority: u8,
+    exchange: String,
+}
+
+impl MxHost {
+    pub fn new(priority: u8, exchange: String) -> Self {
+        Self { priority, exchange }
+    }
+    pub fn priority(&self) -> u8 {
+        self.priority
+    }
+    pub fn exchange(&self) -> String {
+        self.exchange.clone()
+    }
+    /// Returns a reference to the exchange string stored in MxHost
+    pub fn exchange_as_ref(&self) -> &str {
+        self.exchange.as_ref()
+    }
+}
+#[test]
+fn digg_com_mx_host() {
+    let mx_host = MxHost::new(5, String::from("alt1.aspmx.l.google.com."));
+    let ex = mx_host.exchange();
+    let ex_ref = mx_host.exchange_as_ref();
+    assert_eq!(mx_host.priority, 5);
+    assert_eq!(mx_host.exchange, "alt1.aspmx.l.google.com.");
+    assert_eq!(mx_host.exchange, ex);
+    assert_eq!(mx_host.exchange, ex_ref);
+    assert!(ptr::eq(mx_host.exchange_as_ref(), ex_ref));
+}
+
+/// Contains the destination domain and a list of its MX Hosts
+pub struct DomainMxServers {
+    domain: String,
+    mx_hosts: Option<Vec<MxHost>>,
+}
+
+impl DomainMxServers {
+    pub fn new(domain: String, mx_hosts: Option<Vec<MxHost>>) -> Self {
+        Self { domain, mx_hosts }
+    }
+}
+#[test]
+fn example_com_has_no_mx() {
+    let domain = DomainMxServers::new(String::from("example.com"), None);
+    assert_eq!(domain.domain, "example.com");
+}
 pub fn display_mx(mx_response: &ResolveResult<MxLookup>) {
     match mx_response {
         Err(_) => println!("No Records"),

210eda9ebbae107fe4f209e24f7824a7877f5b9a
Adam Spann <Bas-Man@users.noreply.github.com>
2021-04-12 22:14:49 +0900

    code cleanup Removed a bunch of unrequired functions related to returning references when not needed.

--- src/dns/mx.rs
+++ src/dns/mx.rs
@@ -20,36 +20,24 @@ impl MxHost {
     pub fn preference(&self) -> u16 {
         self.preference
     }
-    pub fn preference_as_ref(&self) -> &u16 {
-        &self.preference
-    }
-    pub fn exchange(&self) -> String {
-        self.exchange.clone()
-    }
-    /// Returns a reference to the exchange string stored in MxHost
-    pub fn exchange_as_ref(&self) -> &str {
-        self.exchange.as_ref()
+    pub fn exchange(&self) -> &String {
+        &self.exchange
     }
 }
 #[cfg(test)]
 mod test {
 
     use super::MxHost;
-    use std::ptr;
 
     #[test]
     fn mx_host() {
         let mx_host = MxHost::new(5, String::from("alt1.aspmx.l.google.com."));
-        let ex_ref = mx_host.exchange_as_ref();
-        let pr_ref = mx_host.preference_as_ref();
         // Test preference
         assert_eq!(mx_host.preference, 5);
         assert_eq!(mx_host.preference(), 5);
-        assert_eq!(mx_host.preference_as_ref(), pr_ref);
         // Test Exchange
         assert_eq!(mx_host.exchange, "alt1.aspmx.l.google.com.");
         assert_eq!(mx_host.exchange(), "alt1.aspmx.l.google.com.");
-        assert!(ptr::eq(mx_host.exchange_as_ref(), ex_ref));
     }
 }
 /// Contains the destination domain and a list of its MX Hosts
@@ -60,23 +48,23 @@ pub struct DomainMxServers {
 }
 
 impl DomainMxServers {
-    pub fn new(domain: String, mx_hosts: Option<Vec<MxHost>>) -> Self {
-        Self { domain, mx_hosts }
+    pub fn new(domain: &String) -> Self {
+        Self {
+            domain: domain.clone(),
+            mx_hosts: None,
+        }
     }
-    pub fn new_none(domain: String) -> Self {
-        DomainMxServers::new(domain, None)
+    pub fn new_with_mx(domain: &String, mx_hosts: Option<Vec<MxHost>>) -> Self {
+        Self {
+            domain: domain.clone(),
+            mx_hosts,
+        }
     }
-    pub fn domain(&self) -> String {
-        self.domain.clone()
+    pub fn domain(&self) -> &String {
+        &self.domain
     }
-    pub fn domain_as_ref(&self) -> &str {
-        self.domain.as_ref()
-    }
-    pub fn mx_hosts_as_ref(&self) -> &Option<Vec<MxHost>> {
+    pub fn mx_hosts(&self) -> &Option<Vec<MxHost>> {
         &self.mx_hosts
-    }
-    pub fn mx_hosts(&self) -> Option<Vec<MxHost>> {
-        self.mx_hosts.clone()
     }
     pub fn parse(&mut self, mx_lookup: &ResolveResult<MxLookup>) {
         match mx_lookup {
@@ -96,34 +84,21 @@ impl DomainMxServers {
 mod tests {
     use super::DomainMxServers;
     use super::MxHost;
-    use std::ptr;
 
     #[test]
     fn example_com_has_no_mx() {
-        let domain = DomainMxServers::new(String::from("example.com"), None);
-        let domain_ref = domain.domain_as_ref();
-        let mx_ref = domain.mx_hosts_as_ref();
+        let domain = DomainMxServers::new(&String::from("example.com"));
         assert_eq!(domain.domain, "example.com");
         assert_eq!(domain.domain(), "example.com");
-        assert!(ptr::eq(domain.domain.as_ref(), domain_ref));
-        assert!(ptr::eq(domain.mx_hosts_as_ref(), mx_ref));
-    }
-    #[test]
-    fn test_new_none() {
-        let domain = DomainMxServers::new_none(String::from("example.com"));
-        assert_eq!(domain.domain, "example.com");
-        assert_eq!(domain.mx_hosts.is_none(), true);
     }
     #[test]
     fn example_com_has_mx() {
         let mut mx_hosts = Vec::new();
         mx_hosts.push(MxHost::new(5, String::from("alt1.aspmx.l.google.com.")));
         mx_hosts.push(MxHost::new(10, String::from("alt2.aspmx.l.google.com")));
-        let domain = DomainMxServers::new(String::from("example.com"), Some(mx_hosts));
+        let domain = DomainMxServers::new_with_mx(&String::from("example.com"), Some(mx_hosts));
         assert_eq!(domain.domain, "example.com");
         assert_eq!(domain.mx_hosts.is_some(), true);
-        let mx2 = domain.mx_hosts();
-        assert!(!ptr::eq(&mx2, domain.mx_hosts_as_ref()));
         assert!(!domain.mx_hosts.unwrap().is_empty());
     }
 }
